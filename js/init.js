c753dbe6739b0c43fecdea57c8d22357efe73790
O-Contreras <ocontreras@rsegrupovzla.com>
2018-02-02 06:06:01 -0430

    add

--- js/init.js
+++ js/init.js
@@ -3,6 +3,9 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.slider').slider();
+    $('.carousel').carousel();
+    
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
