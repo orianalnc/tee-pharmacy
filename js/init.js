de72d2a4d6454db28ddb909ba7a80df84bd9e51b
O-Contreras <ocontreras@rsegrupovzla.com>
2018-02-19 20:38:39 -0430

    index, about, contact

--- js/init.js
+++ js/init.js
@@ -5,7 +5,7 @@
     $('.parallax').parallax();
     $('.slider').slider();
     $('.carousel').carousel();
-    
+    $('.modal').modal()
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
