06a5ac5709f0ec48b1118c18c53ef146ea65e53e
Orianalnc <32578277+orianalnc@users.noreply.github.com>
2018-02-25 13:31:40 -0400

    update init

--- js/init.js
+++ js/init.js
@@ -1,11 +1,14 @@
 (function($){
   $(function(){
-
+    Materialize.updateTextFields();
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.slider').slider();
     $('.carousel').carousel();
-    $('.modal').modal()
+    $('.modal').modal();
+      $('#message').val('New Text');
+  $('#message').trigger('autoresize');
+        
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
