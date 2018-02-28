32368b2cc789aa25653ce4a96e0e7490ea1d0ad2
Orianalnc <32578277+orianalnc@users.noreply.github.com>
2018-02-27 22:02:02 -0400

    Update init

--- js/init.js
+++ js/init.js
@@ -6,9 +6,10 @@
     $('.slider').slider();
     $('.carousel').carousel();
     $('.modal').modal();
-      $('#message').val('New Text');
-  $('#message').trigger('autoresize');
-        
+    $('#message').val('New Text');
+    $('#message').trigger('autoresize');
+    $('.collapsible').collapsible();
+   
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
